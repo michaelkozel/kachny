16ab417089ec6d93f029d8977d8adb1d1e052ee6
Hamáček <hamacek@g-chd.local>
2017-12-12 12:24:47 +0100

    Pridan timer a rucni vlozeni tlacitka.

--- zakladkachny/Form1.cs
+++ zakladkachny/Form1.cs
@@ -12,9 +12,31 @@ namespace zakladkachny
 {
     public partial class Form1 : Form
     {
+
+        Button prvni;
+
+        int pocetTicku = 0;
+
         public Form1()
         {
             InitializeComponent();
+            prvni = new Button();
+            prvni.Text = "Kachna";
+            prvni.Left = 100;
+            prvni.Top = 10;
+            prvni.Click += this.Clicked;
+            this.Controls.Add(prvni);
+        }
+
+        public void Clicked(object sender, EventArgs args)
+        {
+            prvni.Text = "Kliknuto";
+            timer.Enabled = true;
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            prvni.Text = "Tick " + pocetTicku++;
         }
     }
 }
