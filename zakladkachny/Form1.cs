6629fad168e556aff4e5e8a8415b1f4258170e76
michael kozel <misakozlik@hotmail.com>
2017-12-19 16:43:54 +0100

    ukol 5

--- zakladkachny/Form1.cs
+++ zakladkachny/Form1.cs
@@ -16,6 +16,7 @@ namespace zakladkachny
         Button prvni;
         Button druhy;
         int odkliknuto = 0;
+        Random random;
         int pocetTicku = 0;
         List<Button> list;
         public Form1()
@@ -33,20 +34,20 @@ namespace zakladkachny
             druhy.Left = 200;
             druhy.Top = 10;
            */
-           
-            Random random = new Random();
+
+            random = new Random();
             timer.Enabled = true;
             list = new List<Button>();
             label1.Text = "Skóre: ";
             for (int i = 0; i < 10; i++)
             {
-                
+
                 druhy = new Button();
                 int pozice = random.Next(label1.Width, ClientSize.Width - druhy.Width);
                 druhy.Click += this.Clicked;
                 druhy.Text = "Kachna";
                 druhy.Left = pozice;
-                druhy.Top = random.Next(-10,15) ;
+                druhy.Top = random.Next(-10, 15);
                 this.Controls.Add(druhy);
                 // zmena druha
                 list.Add(druhy);
@@ -56,25 +57,29 @@ namespace zakladkachny
         public void Clicked(object sender, EventArgs args)
         {
             Button odesilatel = (Button)sender;
-            odesilatel.Visible = false;
+           
             odkliknuto++;
-            label1.Text = "Skóre: "+odkliknuto;
-            if (odkliknuto > 9)
-            { label1.Text = "vyhrál jsi"; }
-            else { label1.Text = "Skóre: " + odkliknuto; }
-            
+            label1.Text = "Skóre: " + odkliknuto;
+          
+              label1.Text = "Skóre: " + odkliknuto; 
+            odesilatel.Top = random.Next(-10, 10);
+            odesilatel.Left = random.Next(label1.Width, ClientSize.Width - druhy.Width);
 
         }
 
         private void timer_Tick(object sender, EventArgs e)
         {
-            for (int i = 0; i < list.Count;i++) {
+            for (int i = 0; i < list.Count; i++)
+            {
                 Button tlacitko = list[i];
-                tlacitko.Top += 2;
-               // prvni.Text = "Tick " + pocetTicku++;
-                if (tlacitko.Top >ClientSize.Height)
-                { tlacitko.Visible = false;
+                tlacitko.Top += 4;
+                // prvni.Text = "Tick " + pocetTicku++;
+                if (tlacitko.Top > ClientSize.Height)
+                {
+                    tlacitko.Top = random.Next(-10, 10);
+                    tlacitko.Left = random.Next(label1.Width, ClientSize.Width - druhy.Width);
                     odkliknuto--;
+                    label1.Text = "Skóre: " + odkliknuto;
                 }
             }
         }
